10c5514efc2a5ae6864a7643bf4fca25d18e1857
skibski <burza.kuba@gmail.com>
2017-03-21 18:31:44 +0100

    broadcasci

---
+++ app/src/main/java/com/burzynski/jakub/broadcascik/MainActivity.java
@@ -0,0 +1,51 @@
+package com.burzynski.jakub.broadcascik;
+
+import android.content.Intent;
+import android.support.annotation.IdRes;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    TextView t1;
+    ImageButton b1, b2, b3, b4, b5, b6, b7, b8, b9;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        t1 = (TextView) findViewById(R.id.t1);
+        b1 = (ImageButton) findViewById(R.id.b1);
+        b2 = (ImageButton) findViewById(R.id.b2);
+        b3 = (ImageButton) findViewById(R.id.b3);
+        b4 = (ImageButton) findViewById(R.id.b4);
+        b5 = (ImageButton) findViewById(R.id.b5);
+        b6 = (ImageButton) findViewById(R.id.b6);
+        b7 = (ImageButton) findViewById(R.id.b7);
+        b8 = (ImageButton) findViewById(R.id.b8);
+        b9 = (ImageButton) findViewById(R.id.b9);
+
+
+
+        t1.setTextColor(0xff9933cc);
+
+        b5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), sms.class);
+                startActivity(i);
+            }
+        });
+
+    }
+
+
+}
---
+++ app/src/main/java/com/burzynski/jakub/broadcascik/sms.java
@@ -0,0 +1,28 @@
+package com.burzynski.jakub.broadcascik;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+/**
+ * Created by student on 21.03.2017.
+ */
+
+public class sms extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sms);
+
+
+
+
+
+
+    }
+
+
+}
