f2dea7035c3a3408e7c3af39a579ee5139e43fd9
skibski <burza.kuba@gmail.com>
2017-03-21 18:51:54 +0100

    broadcasci

--- app/src/main/java/com/burzynski/jakub/broadcascik/sms.java
+++ app/src/main/java/com/burzynski/jakub/broadcascik/sms.java
@@ -1,9 +1,21 @@
 package com.burzynski.jakub.broadcascik;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
 
 /**
  * Created by student on 21.03.2017.
@@ -11,18 +23,72 @@ import android.widget.TextView;
 
 public class sms extends AppCompatActivity {
 
+    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 0;
+    EditText edit1, edit2;
+    Button b10;
+    String phoneNo;
+    String message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sms);
 
+        edit1 = (EditText) findViewById(R.id.edit1);
+        edit2 = (EditText) findViewById(R.id.edit2);
 
+        b10 = (Button) findViewById(R.id.b10);
 
-
-
-
+        b10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendSMSMessage();
+            }
+        });
     }
+
+    protected void sendSMSMessage() {
+        phoneNo = edit1.getText().toString();
+        message = edit2.getText().toString();
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.SEND_SMS)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.SEND_SMS},
+                        MY_PERMISSIONS_REQUEST_SEND_SMS);
+            }
+        }
+    }
+            @Override
+            public void onRequestPermissionsResult(int requestCode,String permissions[], int[] grantResults) {
+                switch (requestCode) {
+                    case MY_PERMISSIONS_REQUEST_SEND_SMS: {
+                        if (grantResults.length > 0
+                                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                            SmsManager smsManager = SmsManager.getDefault();
+                            smsManager.sendTextMessage(phoneNo, null, message, null, null);
+                            Toast.makeText(getApplicationContext(), "SMS sent.",
+                                    Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(getApplicationContext(),
+                                    "SMS faild, please try again.", Toast.LENGTH_LONG).show();
+                            return;
+                        }
+                    }
+                }
+
+
+
+        }
+
+
+
+
+
 
 
 }
